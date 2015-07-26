167776e43d327d229db05e19a8785d8253f5f59d
Vincent San Miguel <vincent.sanmiguel1@gmail.com>
2015-07-26 02:04:15 -0400

    Refactored directories. Status tracking may have been lost

---
+++ cpp/src/include/cavityFlow.h
@@ -0,0 +1,9 @@
+class CavityFlow
+{
+public:
+	CavityFlow();
+	
+	void cavityFlow(int nt, int nit, double u[], double v[], double dt, double dx, double dy, double p[], double rho, double nu, int nx, int ny);
+
+	~CavityFlow();
+};
